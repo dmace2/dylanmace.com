a81ae5be65ba73da087e0271273eba22ebe81e21
Dylan Mace <dtmace2@gmail.com>
2021-12-20 14:13:05 -0500

    new navigation bar

--- assets/js/stickybar.js
+++ assets/js/stickybar.js
@@ -7,11 +7,11 @@ console.log(header);
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
-console.log(sticky);
+// console.log(sticky);
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    
+  console.log("scrolling");
   if (window.pageYOffset > sticky) {
     console.log(window.pageYOffset);
     header.classList.add("sticky");
